bb9ec467d19a1957be09c8dacf40a7996e0f0036
Sandro <sandromedeirosdacunha@gmail.com>
2023-03-15 12:52:22 -0300

    Adicionando HTML + CSS e lógica de número aleatório

---
+++ app/sortearNumero.js
@@ -0,0 +1,14 @@
+
+const maiorValor = 1000;
+const menorValor = 10;
+const numeroAleatorio = gerarNumero();
+
+const elementoMenor = document.getElementById("menor-valor")
+elementoMenor.innerHTML = menorValor
+const elementoMaior = document.getElementById("maior-valor")
+elementoMaior.innerHTML = maiorValor
+
+function gerarNumero(){
+  return parseInt(Math.random() * maiorValor + 1); 
+}
+console.log(numeroAleatorio);
